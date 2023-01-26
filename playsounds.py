649d2c7f4b518b45a37d26ad0171c7da5aad5bcb
PRADHUMAN KUMAR <121790103+pk4727@users.noreply.github.com>
2023-01-27 00:56:48 +0530

    Update playsounds.py

--- playsounds.py
+++ playsounds.py
@@ -1,3 +1,3 @@
 from playsound import playsound
-playsound("p\\k.mp3")     #  p=location of song  , k=name of song  (use \\ in place of \ in python for windows)  
+playsound("p\\k.mp3")     #  p=location of song  , k=name of song  (use \\ in place of \ in python for location in windows)  
 print("playing sound using playsound")
